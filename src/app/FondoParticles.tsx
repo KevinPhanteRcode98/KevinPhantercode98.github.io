d3a17a7cf1007401caaa044a99fdea58a764f650
Kevin Arny Silva Paredes <kevinsilvap1998@hotmail.com>
2026-03-10 16:53:26 -0500

    Versión corregida del portafolio

--- src/app/FondoParticles.tsx
+++ src/app/FondoParticles.tsx
@@ -6,7 +6,7 @@ export default function FondoParticles() {
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    if (!canvas) return;
+    if (!canvas || !(canvas instanceof window.HTMLCanvasElement)) return;
     const ctx = canvas.getContext("2d");
     let width = window.innerWidth;
     let height = window.innerHeight;
